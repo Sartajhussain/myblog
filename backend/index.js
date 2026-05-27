e15365fc920a812253220c79bd5f1caa090b3739
sarta hussain <sartajhusain770@gmail.com>
2026-05-27 12:05:51 +0530

    cors fixed

--- backend/index.js
+++ backend/index.js
@@ -36,7 +36,8 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:5173",
   "http://127.0.0.1:5174",
-  "https://blog-application-774e.onrender.com"
+  "https://myblog.onrender.com",
+  "https://blog-application-774e.onrender.com",
 ];
 
 app.use(
