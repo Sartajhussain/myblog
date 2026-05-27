8f328af844460f4df3245e7cdc23094e81cc6a61
sarta hussain <sartajhusain770@gmail.com>
2026-05-27 12:28:15 +0530

    updated url

--- backend/index.js
+++ backend/index.js
@@ -36,8 +36,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:5173",
   "http://127.0.0.1:5174",
-  "https://myblog-sartaj.onrender.com",
-"https://blog-application-774e.onrender.com",
+  "https://blog-application-774e.onrender.com"
 ];
 
 app.use(
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -28,7 +28,7 @@ const Contact = () => {
       setLoading(true);
 
       const res = await axios.post(
-        "https://myblog.onrender.com/api/v1/contact",
+        "https://blog-application-774e.onrender.com/api/v1/contact",
         form,
         { timeout: 10000 } // optional but good
       );
